d25e5dd9284e5ba25d693e5a3082e28e0d69d3a0
Anthonini <anthonini@gmail.com>
2021-04-07 21:31:51 -0300

    Correções e melhorias na fase do papel.
    - Corrigido o tamanho do segundo piso da fase.
    - Adicionado novo coletável no primeiro piso flutuante.
    - Melhoria no código da definição dos obstáculos e dos coletáveis.
    
    
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- public/fases/fasePapel.js
+++ public/fases/fasePapel.js
@@ -22,23 +22,28 @@ class FasePapel extends Fase {
 
   inicializarObstaculos() {
 	this.obstaculos = [];
-	    
+	
+	//Limites da fase
     this.obstaculos.push(new Obstaculo(0,0,880,1));
     this.obstaculos.push(new Obstaculo(-30,0,30,700));
     this.obstaculos.push(new Obstaculo(875,0,30,700));
     
-    this.obstaculos.push(new Obstaculo(0,130,223,110));
-    this.obstaculos.push(new Obstaculo(710,132,170,18));
+    // Pisos
+    this.obstaculos.push(new Obstaculo(0,545,225,131));
+    this.obstaculos.push(new Obstaculo(340,492,330,190));
+    this.obstaculos.push(new Obstaculo(674,547,108,108));
     
-    this.obstaculos.push(new Obstaculo(495,158,109,9));
-    this.obstaculos.push(new Obstaculo(336,225,109,9));
-    
-    this.obstaculos.push(new Obstaculo(128,345,164,9));
+    //Elevado
     this.obstaculos.push(new Obstaculo(414,378,220,11));
     
-    this.obstaculos.push(new Obstaculo(0,545,225,131));
-    this.obstaculos.push(new Obstaculo(338,492,373,190));
-    this.obstaculos.push(new Obstaculo(676,547,108,108));
+    //Pisos flutuantes
+    this.obstaculos.push(new Obstaculo(495,158,109,9));
+    this.obstaculos.push(new Obstaculo(336,225,109,9));
+    this.obstaculos.push(new Obstaculo(128,345,164,9));
+    
+	//Plataformas flutuantes
+    this.obstaculos.push(new Obstaculo(0,130,220,105));
+    this.obstaculos.push(new Obstaculo(710,132,170,18));
   }
   
   inicializarInimigos() {
@@ -50,12 +55,15 @@ class FasePapel extends Fase {
   inicializarColetaveis() {
     this.coletaveis = [];
     
+    
     this.coletaveis.push(new Coletavel(470,478,this.papelImage));
     this.coletaveis.push(new Coletavel(555,359,this.papelImage));
     this.coletaveis.push(new Coletavel(760,530,this.papelImage));
-    this.coletaveis.push(new Coletavel(186,120,this.papelImage));
+    
+    this.coletaveis.push(new Coletavel(150,328,this.papelImage));
     
     this.coletaveis.push(new Coletavel(35,120,this.papelImage));
+    this.coletaveis.push(new Coletavel(186,120,this.papelImage));
   }
   
   inicializarObjetivo() {
